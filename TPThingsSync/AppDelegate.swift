e034f286721f89bd312b479cd2f53c7a53322036
Adam Lagerhausen <adam@lagerhausen.com>
2016-12-03 09:18:26 -0800

    Initial Commit

---
+++ TPThingsSync/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TPThingsSync
+//
+//  Created by Adam Lagerhausen on 12/3/16.
+//  Copyright © 2016 adam.lagerhausen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TPThingsSync/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TPThingsSync
+//
+//  Created by Adam Lagerhausen on 12/3/16.
+//  Copyright © 2016 adam.lagerhausen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
